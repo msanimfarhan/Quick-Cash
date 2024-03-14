d9e3f841aff065d6007cc3cb56dc91fa31b746ca
Rong <544050376@qq.com>
2024-03-14 10:41:35 -0300

    US1

--- app/src/main/java/com/example/quickcash/employee_landing.java
+++ app/src/main/java/com/example/quickcash/employee_landing.java
@@ -1,11 +1,30 @@
 package com.example.quickcash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+//
+import android.Manifest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import android.location.Location;
+
+
 
 public class employee_landing extends AppCompatActivity{
+
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +32,42 @@ public class employee_landing extends AppCompatActivity{
         setContentView(R.layout.employee_landing);
         Toolbar employeeToolbar = (Toolbar) findViewById(R.id.employee_toolbar);
         setSupportActionBar(employeeToolbar);
+
+        // Initialize FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        checkLocationPermissionAndGetLocation();
+    }
+
+    //
+    private void checkLocationPermissionAndGetLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            getLastLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation();
+        }
+        /*else {
+            //
+
+        }*/
+    }
+    private void getLastLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+
+                        }
+                    }
+                });
     }
 
 
--- app/src/main/java/com/example/quickcash/employer_landing.java
+++ app/src/main/java/com/example/quickcash/employer_landing.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+
+
 public class employer_landing extends AppCompatActivity {
 
     @Override
