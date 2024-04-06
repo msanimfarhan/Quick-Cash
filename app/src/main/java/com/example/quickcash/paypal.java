f1feee125f2e4f3254d4017ce1af3b405ba60aea
purani <sm841586@dal.ca>
2024-04-06 12:06:47 -0300

    Toolbar working

--- app/src/main/java/com/example/quickcash/paypal.java
+++ app/src/main/java/com/example/quickcash/paypal.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -32,6 +34,8 @@ public class paypal extends AppCompatActivity {
     private AppCompatEditText payamount;
     private AppCompatButton paypalpaybtn;
     private AppCompatTextView paymentStatus;
+    private Button jobBoard;
+    private Button notification;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +44,34 @@ public class paypal extends AppCompatActivity {
         configpaypal();
         initActivityLauncher();
         setListeners();
+        setupjobBoardListener();
+        setupNotificationListener();
     }
 
     private void init(){
         payamount = findViewById(R.id.amount);
         paypalpaybtn = findViewById(R.id.paypalpaybtn);
         paymentStatus = findViewById(R.id.PaymentStatus);
+        jobBoard = findViewById(R.id.job_board_btn);
+        notification = findViewById(R.id.notification_btn);
+    }
+
+    private void setupjobBoardListener(){
+        jobBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move2JobPosting();
+            }
+        });
+    }
+
+    private void setupNotificationListener() {
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move2Notification();
+            }
+        });
     }
 
     private void configpaypal() {
@@ -55,6 +81,15 @@ public class paypal extends AppCompatActivity {
 
     }
 
+    protected void move2JobPosting() {
+        Intent paypalIntent = new Intent(this, job_Posting.class);
+        startActivity(paypalIntent);
+    }
+
+    protected void move2Notification(){
+        Intent paypalIntent = new Intent(this, Notification.class);
+        startActivity(paypalIntent);
+    }
     private void initActivityLauncher() {
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 result -> {
